4f581934730ae9db063e4232fd6f0a359eceb931
Rhys Talley <rhyslestertalley@gmail.com>
2024-08-19 17:48:12 -0400

    score dynamically updates and teams are sorted

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+    scoreboard.sort(reverse=True, key=lambda d: d["score"])
     return jsonify(scoreboard=scoreboard)
 
 
